6b0e16cca5cc8dbef40e2ab63a687f9224866285
taojian <1099593012@qq.com>
2018-05-05 23:26:12 +0800

    反转链表

---
+++ AlgorithmSource/src/main/java/com/offer/demo/ConstuctTree.java
@@ -0,0 +1,18 @@
+package com.offer.demo;
+
+/**
+ * 新建二叉树
+ * 知道中序和先序，重建二叉树并输出头部
+ */
+public class ConstuctTree {
+
+
+
+}
+
+
+class  TreeNode{
+    int data;
+    TreeNode rightNode;
+    TreeNode leftNode;
+}
--- README.md
+++ README.md
@@ -8,4 +8,6 @@ write a summary of algorithm
 ## 剑指offer
 ---
 1.单例模式</p>
-2.二维数组的查找
+2.二维数组的查找</p>
+3.空格替换 </p>
+4.反转链表 </p>
