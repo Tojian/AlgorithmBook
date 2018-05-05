f2339d5ae2bbf118b2a7fd5a5d5cce37651aa62e
taojian <1099593012@qq.com>
2018-05-05 22:58:07 +0800

    两个有序的数组进行排序

---
+++ AlgorithmSource/src/main/java/com/offer/demo/CompareArrayOfTwo.java
@@ -0,0 +1,46 @@
+package com.offer.demo;
+
+/**
+ * 两个有序数组，A ，B ，A的末尾有足够多的长度，B的数组的数字插入A的数组里面，保证有序
+ */
+public class CompareArrayOfTwo {
+    public static void main(String[] args) {
+
+        int [] a = new int[20];
+        int [] b = {0, 3,5,7,9};
+
+        int [] revert = sortCombinedArrayOfTwo(a , b);
+        for (int i = 0 ; i < revert.length ;i++){
+            System.out.print(revert[i] + " ");
+        }
+    }
+
+    public static int[] sortCombinedArrayOfTwo(int [] a , int [] b){
+        if (a.length < b.length)
+            return  null;
+
+        int aPoint = 0;
+        for (int j = 0 ; j < a.length ; j++){
+            if(a[j]!=0){
+                aPoint ++;
+            }
+        }
+        int bPoint = b.length -1 ;
+
+        while (aPoint > 0 && bPoint >=0){
+
+            if (a[aPoint] > b[bPoint]){
+                a[aPoint + b.length] = a[aPoint];
+                aPoint --;
+            }else {
+                a[aPoint + b.length] = b[bPoint];
+                bPoint --;
+            }
+        }
+        if (bPoint > 0){
+            for (int i = 0;i < bPoint ;i++)
+                a[i] = b[i];
+        }
+        return a;
+    }
+}
