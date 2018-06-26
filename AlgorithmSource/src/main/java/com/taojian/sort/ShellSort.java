87d5bee6a8678e6bcb99b22c83dd758c0056edee
taojian <1099593012@qq.com>
2018-06-26 11:20:42 +0800

    shell sort

---
+++ AlgorithmSource/src/main/java/com/taojian/sort/ShellSort.java
@@ -0,0 +1,52 @@
+package com.taojian.sort;
+
+import java.util.Arrays;
+
+/**
+ * @description: 希尔排序
+ * @author: taojian
+ * @create: 2018-06-25 20:13
+ * 1959年Shell发明，第一个突破O(n2)的排序算法，是简单插入排序的改进版。它与插入排序的不同之处在于，它会优先比较距离较远的元素。希尔排序又叫缩小增量排序。
+ *
+ * 4.1 算法描述
+ * 先将整个待排序的记录序列分割成为若干子序列分别进行直接插入排序，具体算法描述：
+ *
+ * 选择一个增量序列t1，t2，…，tk，其中ti>tj，tk=1；
+ * 按增量序列个数k，对序列进行k 趟排序；
+ * 每趟排序，根据对应的增量ti，将待排序列分割成若干长度为m 的子序列，
+ * 分别对各子表进行直接插入排序。仅增量因子为1 时，整个序列作为一个表来处理，表长度即为整个序列的长度。
+ **/
+public class ShellSort {
+
+    public static void sort(int[] array) {
+        int inner, outer;
+        int temp;
+
+        int h = 1;
+        while (h <= array.length / 3) {
+            h = h * 3 + 1;
+        }
+        while (h > 0) {
+            for (outer = h; outer < array.length; outer++) {
+                temp = array[outer];
+                inner = outer;
+
+                while (inner > h - 1 && array[inner - h] >= temp) {
+                    array[inner] = array[inner - h];
+                    inner -= h;
+                }
+                array[inner] = temp;
+            }
+            h = (h - 1) / 3;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int [] array = {5,3,0,2,4,1,0,5,2,3,1,4};
+        System.out.println("Before: " + Arrays.toString(array));
+        sort(array);
+        System.out.println("After:  " + Arrays.toString(array));
+
+    }
+}
