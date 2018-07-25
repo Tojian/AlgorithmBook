b5e4ced456ed5a1c48c04f100d4ceb180cb17388
taojian <1099593012@qq.com>
2018-07-25 21:34:47 +0800

    array

---
+++ AlgorithmSource/src/main/java/com/taojian/array/MinNumberInRotateArray.java
@@ -0,0 +1,41 @@
+package com.taojian.array;
+
+/**
+ * @description:
+ * @author: taojian
+ * @create: 2018-07-16 21:13
+ *
+ * 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。输入一个非递减排序的数组的一个旋转，输出旋转数组的最小元素。例如数组 {3, 4, 5, 1, 2} 为 {1, 2, 3, 4, 5}
+ * 的一个旋转，该数组的最小值为 1。NOTE：给出的所有元素都大于 0，若数组大小为 0，请返回 0。
+ *
+ * 解题思路
+ * 当 nums[m] <= nums[h] 的情况下，说明解在 [l, m] 之间，此时令 h = m；否则解在 [m + 1, h] 之间，令 l = m + 1。
+ *
+ * 因为 h 的赋值表达式为 h = m，因此循环体的循环条件应该为 l < h
+ *
+ * 复杂度：O(logN) + O(1)
+ *
+ *
+ * 对于旋转数组，使用二分法来查找最小元素。
+ * 取中间mid元素，如果array[mid]>array[end]，说明最小元素应该在mid的右边，因为我们实际是要找没有旋转之前的第一个元素，
+ * 观察数组规律可以知道，在mid的右边
+ * 如果array[mid]<array[start]，说明已经取到了被旋转的那一节数组了，因此此时的最小元素，应该在mid的左边
+ * 另外有一个特殊情况，就是分不清楚在哪段数组中，也就是恰好有重复元素，那就只能遍历查找了，所以最坏的时间复杂度是O(n)，最好是O(logn)
+ **/
+public class MinNumberInRotateArray {
+
+
+    public int minNumberInRotateArray(int[] nums) {
+        if (nums.length == 0)
+            return 0;
+        int l = 0, h = nums.length - 1;
+        while (l < h) {
+            int m = l + (h - l) / 2;
+            if (nums[m] <= nums[h])
+                h = m;
+            else
+                l = m + 1;
+        }
+        return nums[l];
+    }
+}
--- AlgorithmSource/src/main/java/com/taojian/sort/MergeSort.java
+++ AlgorithmSource/src/main/java/com/taojian/sort/MergeSort.java
@@ -6,11 +6,64 @@ package com.taojian.sort;
  * @create: 2018-06-26 11:31
  * 归并排序是建立在归并操作上的一种有效的排序算法。该算法是采用分治法（Divide and
  * Conquer）的一个非常典型的应用。将已有序的子序列合并，得到完全有序的序列；即先使每个子序列有序，再使子序列段间有序。若将两个有序表合并成一个有序表，称为2-路归并。
- *
+ * <p>
  * 5.1 算法描述
  * 把长度为n的输入序列分成两个长度为n/2的子序列；
  * 对这两个子序列分别采用归并排序；
  * 将两个排序好的子序列合并成一个最终的排序序列。
  **/
 public class MergeSort {
+
+    public static int[] sort(int[] nums, int low, int high) {
+        int mid = (low + high) / 2;
+        if (low < high) {
+            // 左边
+            sort(nums, low, mid);
+            // 右边
+            sort(nums, mid + 1, high);
+            // 左右归并
+            merge(nums, low, mid, high);
+
+        }
+        return nums;
+
+    }
+
+    public static void merge(int[] nums, int low, int mid, int high) {
+        int[] temp = new int[high - low + 1];
+        int i = low;// 左指针
+        int j = mid + 1;// 右指针
+        int k = 0;
+
+        // 把较小的数先移到新数组中
+        while (i <= mid && j <= high) {
+            if (nums[i] < nums[j]) {
+                temp[k++] = nums[i++];
+
+            } else {
+                temp[k++] = nums[j++];
+
+            }
+
+        }
+        // 把左边剩余的数移入数组
+        while (i <= mid) {
+            temp[k++] = nums[i++];
+
+        }
+
+        // 把右边边剩余的数移入数组
+        while (j <= high) {
+            temp[k++] = nums[j++];
+
+        }
+
+        // 把新数组中的数覆盖nums数组
+        for (int k2 = 0; k2 < temp.length; k2++) {
+            nums[k2 + low] = temp[k2];
+
+        }
+
+    }
+
 }
