361b120c984d26c0283e0ad7f4a15dc21d7efefd
taojian <1099593012@qq.com>
2018-07-28 16:27:40 +0800

    search insert position

--- AlgorithmSource/src/main/java/com/taojian/array/binarysearch/easy/SearchInsertPosition.java
+++ AlgorithmSource/src/main/java/com/taojian/array/binarysearch/easy/SearchInsertPosition.java
@@ -4,28 +4,48 @@ package com.taojian.array.binarysearch.easy;
  * @description:
  * @author: taojian
  * @create: 2018-07-27 15:48
- *
+ * <p>
  * Given a sorted array and a target value, return the index if the target is found. If not, return the index where
  * it would be if it were inserted in order.
- *
+ * <p>
  * You may assume no duplicates in the array.
- *
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: [1,3,5,6], 5
  * Output: 2
  * Example 2:
- *
+ * <p>
  * Input: [1,3,5,6], 2
  * Output: 1
  * Example 3:
- *
+ * <p>
  * Input: [1,3,5,6], 7
  * Output: 4
  * Example 4:
- *
+ * <p>
  * Input: [1,3,5,6], 0
  * Output: 0
  **/
 public class SearchInsertPosition {
+
+    public int searchInsert(int[] nums, int target) {
+        if (nums.length < 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        int mid = 0;
+        while (left <= right) {
+            mid = (left - right) / 2 + right;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
 }
