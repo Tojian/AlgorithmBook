7603ec02bb6de3ffb9c12146ca92673673a2869c
taojian <1099593012@qq.com>
2018-07-29 10:19:02 +0800

    binary search

--- AlgorithmSource/src/main/java/com/taojian/array/binarysearch/easy/ArrangingCoins.java
+++ AlgorithmSource/src/main/java/com/taojian/array/binarysearch/easy/ArrangingCoins.java
@@ -35,4 +35,33 @@ package com.taojian.array.binarysearch.easy;
  * Because the 4th row is incomplete, we return 3.
  **/
 public class ArrangingCoins {
+
+
+    public int arrangeCoins(int n) {
+        if (n <= 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        long left = 1;
+        long right = n;
+        while (left <= right){
+            long mid = (left -right)/2 +right;
+            if ((mid*(mid+1)/2) > n && (mid*(mid+1)/2) - mid <= n)
+                return (int)(mid - 1) ;
+            else if((mid*(mid+1)/2) > n && (mid*(mid+1)/2) - mid > n  )
+                right = mid - 1;
+            else left = mid + 1;
+        }
+        return -1;
+    }
+
+    public int arrangeCoins1(int n) {
+        if(n < 1)
+            return 0;
+        for(int i = 1; ;i++){
+            n -=i;
+            if(n < 0)
+                return i-1;
+        }
+    }
 }
