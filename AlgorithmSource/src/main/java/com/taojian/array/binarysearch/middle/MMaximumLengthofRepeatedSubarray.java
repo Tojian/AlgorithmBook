1357ca09f600a2a2acfaafa1fb9e875eb5d4d04d
taojian <1099593012@qq.com>
2018-07-29 14:49:24 +0800

    binary search

---
+++ AlgorithmSource/src/main/java/com/taojian/array/binarysearch/middle/MMaximumLengthofRepeatedSubarray.java
@@ -0,0 +1,23 @@
+package com.taojian.array.binarysearch.middle;
+
+/**
+ * @description:
+ * @author: taojian
+ * @create: 2018-07-29 14:26
+ *
+ *
+ * Given two integer arrays A and B, return the maximum length of an subarray that appears in both arrays.
+ *
+ * Example 1:
+ * Input:
+ * A: [1,2,3,2,1]
+ * B: [3,2,1,4,7]
+ * Output: 3
+ * Explanation:
+ * The repeated subarray with maximum length is [3, 2, 1].
+ * Note:
+ * 1 <= len(A), len(B) <= 1000
+ * 0 <= A[i], B[i] < 100
+ **/
+public class MMaximumLengthofRepeatedSubarray {
+}
