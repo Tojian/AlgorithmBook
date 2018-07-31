52a333acd319a9d5989f1ef36fc78cbb697111a8
taojian <1099593012@qq.com>
2018-07-31 15:41:57 +0800

    binary search middle

---
+++ AlgorithmSource/src/main/java/com/taojian/array/binarysearch/middle/FindPeakElement.java
@@ -0,0 +1,47 @@
+package com.taojian.array.binarysearch.middle;
+
+/**
+ * @description:
+ * @author: taojian
+ * @create: 2018-07-31 15:04
+ * <p>
+ * Find Peak Element
+ * <p>
+ * A peak element is an element that is greater than its neighbors.
+ * <p>
+ * Given an input array nums, where nums[i] ≠ nums[i+1], find a peak element and return its index.
+ * <p>
+ * The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+ * <p>
+ * You may imagine that nums[-1] = nums[n] = -∞.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [1,2,3,1]
+ * Output: 2
+ * Explanation: 3 is a peak element and your function should return the index number 2.
+ * Example 2:
+ * <p>
+ * Input: nums = [1,2,1,3,5,6,4]
+ * Output: 1 or 5
+ * Explanation: Your function can return either index number 1 where the peak element is 2,
+ * or index number 5 where the peak element is 6.
+ * Note:
+ * <p>
+ * Your solution should be in logarithmic complexity.
+ **/
+public class FindPeakElement {
+
+    public int findPeakElement(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+}
